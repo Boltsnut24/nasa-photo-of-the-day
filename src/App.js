e9eb5994b6e4fd685406b3fc36eb67e723b0a9f4
Brad <Brad@BRADSPC>
2019-07-17 17:00:26 -0600

    Doesnt compile yet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+  axios
+    .get('http://api.nasan.gov/planetary/apod?api_key=DEMO_KEY')
+    .then(res => {
+      console.log(res.data);
+    });
+
+
+return (
+  <div className="App">
+    <p>
+      Read through the instructions in the README.md file to build your NASA
+      app! Have fun 🚀!
       </p>
-    </div>
-  );
-}
+  </div>
+);
+
 
 export default App;
